4e57c8a85cd29ff87d2bef5470ffe6614d09dac8
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-10 19:48:48 +0530

    ::useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [color , setColor] = useState("yellow")
+
+  function changeColor(color) {
+     setColor(color)
+  }
+
   return (
-    <div className='w-full h-screen duration-200'>
+    <div className='w-full h-screen duration-200' style={{backgroundColor : color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
-           <button>test</button>
+          
+           <button 
+             className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
+             onClick={()=> changeColor("red")}>
+              Red
+           </button>
+
+           
+
          </div>
       </div>
     </div>
