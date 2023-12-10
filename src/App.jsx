4badba7805b328364c5d78eb12e8b37fe04195e2
Yatik Srivastava <yatiksrivastava1@gmail.com>
2023-12-10 19:54:51 +0530

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,24 @@ const App = () => {
 
   const [color , setColor] = useState("yellow")
 
-  // function changeColor(color) {
-  //    setColor(color)
-  // }
+  
 
   return (
     <div className='w-full h-screen duration-200' style={{backgroundColor : color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+         <div className="flex flex-wrap justify-center gap-3 font-bold shadow-lg bg-white px-3 py-2 rounded-3xl">
           
            <button 
              className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
-             onClick={()=> setColor("red")}>
+             onClick={()=> setColor("red")}
+             style={{backgroundColor : "red", color: "white"}}>
               Red
            </button>
 
            <button 
              className='outline-none px-4 py-1 rounded-full shadow-lg text-black'
-             onClick={() => setColor("green")}>
+             onClick={() => setColor("green")}
+             style={{backgroundColor: "green"}}>
                Green
            </button>
 
